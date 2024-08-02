16c9945d3b1c34ae25ef8b121ebcb05a9dd59941
bigdropincs <5964698+bigdropincs@users.noreply.github.com>
2024-08-02 15:21:25 -0700

    more code housekeeping

---
+++ apps/nextjs/src/app/(marketing)/_components/footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export const Footer = () => (
+  <footer>
+    <div className="mx-auto max-w-7xl border-x border-b border-border p-8 lg:border-b-0">
+      <div className="text-muted-foreground">Kit.</div>
+      <div className="mt-28 grid grid-cols-1 lg:grid-cols-2">
+        <p className="mt-4 text-xl font-light text-secondary-foreground">
+          Accelerate building modern applications
+        </p>
+        <div className="mt-4 grid gap-2 md:grid-cols-2">
+          <ul>
+          </ul>
+          <ul>
+            <li>
+              <Link
+                className="text-sm text-muted-foreground transition hover:text-secondary-foreground"
+                href="/docs"
+              >
+                Documentation
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-sm text-muted-foreground transition hover:text-secondary-foreground"
+                href="https://github.com/bigdropincs/kit/blob/main/LICENSE"
+                target="_blank"
+              >
+                Licensing
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
