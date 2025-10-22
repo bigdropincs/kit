6a6ad982750ed8ab650288783210aeae9153b931
bigdropincs <5964698+bigdropincs@users.noreply.github.com>
2025-10-22 11:04:48 -0700

    upgrade to next 16

--- apps/expo/eslint.config.ts
+++ apps/expo/eslint.config.ts
@@ -1,7 +1,6 @@
+import { baseConfig } from "@bigdropincs/eslint-config/base";
+import { reactConfig } from "@bigdropincs/eslint-config/react";
 import { defineConfig } from "eslint/config";
-
-import { baseConfig } from "@acme/eslint-config/base";
-import { reactConfig } from "@acme/eslint-config/react";
 
 export default defineConfig(
   {
